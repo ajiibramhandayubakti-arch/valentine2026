b1b7341c9acb9d7408c0d14be1876ecf7152183d
ajiibramhandayubakti-arch <ajiibramhandayubakti.2024@gmail.com>
2026-02-13 00:35:43 +0700

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,17 +1,1 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyB_SYE0QM4mzykxZPLpKvq09ake4p1dEz0",
-  authDomain: "valentineproject-bram.firebaseapp.com",
-  projectId: "valentineproject-bram",
-  storageBucket: "valentineproject-bram.firebasestorage.app",
-  messagingSenderId: "21524686863",
-  appId: "1:21524686863:web:a126ceef15bfe17962db7a",
-  measurementId: "G-7FSLNBKQ56"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-// 🔥 WAJIB GLOBAL
-window.auth = firebase.auth();
-window.db = firebase.firestore();
-
-console.log("Firebase + Firestore READY");
+alert("firebase.js KELOAD");
